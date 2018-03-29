0608f542ebb1d0fb675a87eb1694e0ae8bb27786
a21220528 <a21220528@DESKTOP-3PPC1QF>
2018-03-29 02:19:37 +0100

    Criação de classe para uma carta de eventos Ainda falta resolver como é que a classe evento irá informar os seus efeitos.

--- src/Engine/EventCard.java
+++ src/Engine/EventCard.java
@@ -5,10 +5,62 @@
  */
 package Engine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author a21220528
  */
 public class EventCard {
-    
+
+    private int id;
+    private final List<Event> events;
+
+    EventCard() {
+        this.events = new ArrayList<>();
+    }
+
+    boolean addEvent(Event eventInput) {
+        if (this.events.size() < 3) {
+            this.events.add(eventInput);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String toPrint = "";
+        for (Event event : events) {
+            toPrint += "----------\nName: " + event.getName() + "\nDescription: ";
+            for (int i = 0; i < event.getDescription().size(); i++) {
+                toPrint += event.getDescription().get(i) + "\n";
+            }
+            toPrint += "Movement:";
+            if (!event.getMovement().isEmpty()) {
+                for (int i = 0; i < event.getMovement().size(); i++) {
+                    switch (event.getMovement().get(i)) {
+                        case Event.LADDERS_MOVEMENT:
+                            toPrint += " Ladders";
+                            break;
+                        case Event.BATTERING_RAM_MOVEMENT:
+                            toPrint += " Battering Ram";
+                            break;
+                        case Event.SIEGE_TOWER_MOVEMENT:
+                            toPrint += " Siege Tower";
+                            break;
+                        case Event.SWORD_MOVEMENT:
+                            toPrint += " Sword";
+                            break;
+                    }
+                }
+            } else {
+                toPrint += " None";
+            }
+            toPrint += "\nAction Point Allowance: " + event.getActionPointAllowance() + "\n----------\n";
+        }
+        return toPrint;
+    }
+
 }
